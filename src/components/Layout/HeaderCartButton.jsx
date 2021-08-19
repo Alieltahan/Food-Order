4edb71445afb4ec6b587364c3f75bb2824583515
Alieltahan <alyaltahan@gmail.com>
2021-08-19 18:03:12 +0200

    CartContext

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,37 @@
-import React, { Fragment } from "react";
+import React, { useContext, Fragment, useReducer } from "react";
 import CartIcon from "./../Cart/CartIcon";
 import styles from "../styles/HeaderCartButton.module.css";
+import ContextCart from "./../store/context-Cart";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  return defaultCartState;
+};
 
 const HeaderCartButton = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const ctxCart = useContext(ContextCart);
+
+  const numberOfItems = ctxCart.items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+
   return (
     <Fragment>
-      <button className={styles.button}>
+      <button onClick={props.onOShowCart} className={styles.button}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={styles.badge}>3</span>
+        <span className={styles.badge}>{numberOfItems}</span>
       </button>
     </Fragment>
   );
