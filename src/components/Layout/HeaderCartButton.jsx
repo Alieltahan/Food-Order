5cea12ea432f6bf26e29ba5df3874153d2127e29
Alieltahan <alyaltahan@gmail.com>
2021-08-21 11:27:32 +0200

    Cart Btn Effect

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -5,10 +5,27 @@ import ContextCart from "./../store/context-Cart";
 
 const HeaderCartButton = (props) => {
   const ctxCart = useContext(ContextCart);
+  const [btnHighlight, setBtnHighlight] = useState(false);
 
+  const { items } = ctxCart;
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnHighlight(true);
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+  const numberOfItems = ctxCart.items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+
+  const btnClasses = `${styles.button} ${btnHighlight ? styles.bump : ""}`;
   return (
     <Fragment>
-      <button onClick={props.onOShowCart} className={styles.button}>
+      <button onClick={props.onOShowCart} className={btnClasses}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
